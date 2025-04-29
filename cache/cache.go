53e6ca4781ea876f607cdfc1ddd2f9174f4fc4ec
Kaycee Nuel <heykayceee@gmail.com>
2025-04-29 23:29:23 +0100

    added Get operation to fetch items

--- cache/cache.go
+++ cache/cache.go
@@ -15,9 +15,9 @@ type Cache struct {
 }
 
 // Pair stores key-value pairs
-type pair struct {
-	key   int
-	value int
+type Pair struct {
+	key   string
+	value interface{}
 }
 
 // NewCache initializes the LRUCache with a given capacity
@@ -27,4 +27,16 @@ func NewCache(capacity int) *Cache {
 		cache:    make(map[string]*list.Element),
 		list:     list.New(),
 	}
+}
+
+// "Get" retrieves a value the cache
+func (c *Cache) Get(key string) (interface{}, bool) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	if elem, ok := c.cache[key]; ok {
+		c.list.MoveToFront(elem)
+		return elem.Value.(*Pair).value, true
+	}
+	return nil, false
+
 }
