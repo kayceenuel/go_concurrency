4a7aaf8d8aad0443fd4de29e726cb8a10f6bcf44
Kaycee Nuel <heykayceee@gmail.com>
2025-04-29 22:30:02 +0100

    implementing LRU cache struct

--- cache/cache.go
+++ cache/cache.go
@@ -1,5 +1,21 @@
 package cache
 
-func NewCache[k comparabale, V any](entryLimit int) Cache[K, V] {
-	return Cache[K, V]{}
+import (
+	"container/list"
+	"sync"
+)
+
+// Cache represent a thread safe LRU cache
+type Cache struct {
+	mu       sync.Mutex               //protects the cache form concurrency access
+	capacity int                      //Maximum capacity of the cache
+	cache    map[string]*list.Element // Maps key to elements
+	list     *list.List               // Doubly linked list LRU order
+
+}
+
+// Pair stores key-value pairs
+type pair struct {
+	key   int
+	value int
 }
