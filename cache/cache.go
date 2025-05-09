935e10b5aae3f9cf72b980eee5bc227ef46cc15f
Kaycee Nuel <heykayceee@gmail.com>
2025-05-09 08:44:10 +0100

    added the put value to the cache

--- cache/cache.go
+++ cache/cache.go
@@ -5,7 +5,7 @@ import (
 )
 
 // Cache represents a thread safe LRU cache
-type Cahce[k comparable, V any] struct {
+type Cache[k comparable, V any] struct {
 	mu         sync.Mutex           // Mutex for thread safety
 	entryLimit int                  // Max num of entries in cache
 	items      map[k]*entry[V]      // map of cache entries
@@ -21,11 +21,56 @@ type entry[V any] struct {
 }
 
 // NewCahce creates a new LRU cache with the given entry limit
-func NewCahce[k comparable, V any](entryLimit int) *Cahce[k, V] {
-	return &Cahce[k, V]{
+func NewCahce[k comparable, V any](entryLimit int) *Cache[k, V] {
+	return &Cache[k, V]{
 		entryLimit: entryLimit,
 		items:      make(map[k]*entry[V]),
 		lruList:    newDoublyLinkedList[k](),
 		stats:      newStatistics(),
 	}
+}
+
+// Put adds the value to the cache, and returns a boolean to indicate whether
+// a value already existed in the cache fo that key
+func (c *Cache[K, V]) Put(key K, value V) bool {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	c.stats.IncrementWrites()
+
+	// check if the key already exists
+	existingEntry, exists := c.items[key]
+	if exists {
+		// Updata the exisiting key
+		existingEntry.value = value
+		existingEntry.readAfterWrite = false
+		// move to front of LRU list (most recently used)
+		c.lruList.moveTofront(key)
+		return true
+	}
+
+	// key doesn't exist, add it to the cache
+	// if we're at capacity, remove the least recently used item
+
+	if len(c.items) >= c.entryLimit && len(c.items) > 0 {
+		lruKey := c.lruList.removeLast() // Remove from linked list
+
+		// Update stats before removing
+		if !c.items[lruKey].readAfterWrite {
+			c.stats.IncrementNeverRead()
+		}
+
+		delete(c.items, lruKey) // Remove from map
+		c.stats.IncrementEvictions()
+	}
+
+	// Add the new entry
+	c.items[key] = &entry[V]{
+		value:          value,
+		accessCount:    0,
+		readAfterWrite: false,
+	}
+	c.lruList.addToFront(key)
+
+	return false
 }
