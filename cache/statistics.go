90fe16358cf43a5dd09ae4caed76b49b92cebb74
Kaycee Nuel <heykayceee@gmail.com>
2025-05-12 20:58:08 +0100

    added func for Writes and Hits stats tracker

--- cache/statistics.go
+++ cache/statistics.go
@@ -6,7 +6,9 @@ import (
 
 // Statistics tracks cache usage statistics
 type Statistics struct {
-	Reads int64 // Total number of read operations
+	Reads  int64 // Total number of read operations
+	Writes int64 // Total number of write operations
+	Hits   int64 // Number of cache hits
 }
 
 // newStatistics creates a new statistics tracker
@@ -16,4 +18,12 @@ func newStatistics() *Statistics {
 
 func (s *Statistics) IncrementReads() {
 	atomic.AddInt64(&s.Reads, 1)
+}
+
+func (s *Statistics) IncrementWrites() {
+	atomic.AddInt64(&s.Writes, 1)
+}
+
+func (s *Statistics) IncrementHits() {
+	atomic.AddInt64(&s.Hits, 1)
 }
