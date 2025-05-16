8986414d74da57103a1504b84af49d0929b770d3
Kaycee Nuel <heykayceee@gmail.com>
2025-05-16 19:30:58 +0100

    Added test cases for cache ops and overwriting exisiting values, LRU eviction, and most recently used items

--- cache/cache_test.go
+++ cache/cache_test.go
@@ -20,4 +20,42 @@ func TestBasicCacheOperations(t *testing.T) {
 	if *val != 1 {
 		t.Errorf("Expected value 1, got %d", *val)
 	}
+
+	// Test overwriting existing value
+	existed = cache.Put("one", 100)
+	if !existed {
+		t.Errorf("Key should exist on second Put")
+	}
+
+	val, found = cache.Get("one")
+	if !found {
+		t.Error("Key should exist one second Put")
+	}
+	if *val != 100 {
+		t.Errorf("Expected updated value 100, got %d", *val)
+	}
+
+	// Test LRU eviction
+	cache.Put("two", 2)
+	cache.Put("three", 3)
+	cache.Put("four", 4) // Should evict "one" from cache
+
+	_, found = cache.Get("one")
+	if found {
+		t.Error("Key 'one' should have been evicted")
+	}
+
+	// Test that most recenlty items are kept
+	cache.Get("two")     // Access "two" to make it most recently used
+	cache.Put("five", 5) // should evict "three"
+
+	_, found = cache.Get("three")
+	if found {
+		t.Error("Key 'three' should have been evicted")
+	}
+
+	_, found = cache.Get("two")
+	if !found {
+		t.Error("Key 'two' should not have been evicted")
+	}
 }
